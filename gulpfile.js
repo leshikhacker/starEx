70da60663abdd1e206e644b1a8772d4350ad677f
leshikhacker <leshikhacker@gmail.com>
2018-04-22 02:27:30 +0300

    first crud  version of responsive markup

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,35 @@ var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+var mqpacker = require('css-mqpacker');
 var postcssImport  = require("postcss-import");
-var cssvariables = require('postcss-css-variables');
 
-gulp.task('serve', [], function() {
+
+gulp.task('serve', ['css'], function() {
+
   browserSync.init({
     server: {
       baseDir: "./"
-    }
+    },
+    port: 3010
   });
-  
+
   gulp.watch("./css/postcss/*.pcss", ['css']);
   gulp.watch("./css/postcss/**/*.pcss", ['css']);
-
   gulp.watch("./js/*.js").on('change', browserSync.reload);
+  gulp.watch("./images/*").on('change', browserSync.reload);
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
 
 gulp.task('css', function () {
   return gulp.src('./css/postcss/main.pcss')
     .pipe(postcss([
       postcssImport(),
-      autoprefixer({browsers: ['last 1 version']}),
-      cssvariables
+      precss,
+      autoprefixer({browsers: ['last 2 versions']}),
+      mqpacker
     ]))
     .pipe(rename({
       extname: ".css"
@@ -35,3 +41,5 @@ gulp.task('css', function () {
 });
 
 gulp.task('default', ['serve']);
+
+
